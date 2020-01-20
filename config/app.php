b55661b20500d7243530f3b5464766e6c323ad90
davidhenrie700 <david.henrique700@gmail.com>
2020-01-20 14:13:21 -0300

    env

--- config/app.php
+++ config/app.php
@@ -121,8 +121,8 @@ return [
 
     'key' => env('APP_KEY'),
 
-    // 'cipher' => 'AES-256-CBC',
-    'cipher' => 'AES-128-CBC',
+    'cipher' => 'AES-256-CBC',
+    
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
